e3d0d150407dc9bdc5f1b611a196cf636689f6b1
alienunderling@hotmail.com <alienunderling@hotmail.com>
2017-06-03 17:17:32 -0600

    Added Visual Studio files

---
+++ BattleShipApplication/BattleShipApplication/Program.cs
@@ -0,0 +1,18 @@
+﻿using BattleShipApplication.Components.Game;
+using System;
+
+namespace BattleShipApplication
+{
+    class BattleShip
+	{
+		static void Main(string[] args)
+		{
+			BattleShipGame game = new BattleShipGame();
+			/* Battleship Runner */
+			Console.WriteLine("Ready to go, Players?  Lets place our ships! Press any key to continue.");
+			Console.ReadKey();
+			Console.WriteLine("Game Starting...");
+			game.startGame();
+		}
+	}
+}
